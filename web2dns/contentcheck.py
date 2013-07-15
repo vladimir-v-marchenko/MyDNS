b74f11cf0e0200b3d7074c535cb24b05a0c4d8df
Sven Velt <sven@velt.de>
2013-07-15 10:54:59 +0200

    AAAA und TXT als RR-Typen dazu

--- web2dns/contentcheck.py
+++ web2dns/contentcheck.py
@@ -1,16 +1,35 @@
 # -*- encoding: utf-8 -*-
 
+import re
 import socket
+
+
+re_txt = re.compile(r'^[ "0-9a-zA-Z_!?&+-]+$')
+
+
 
 def contentcheck(rrtype, content):
 	rrtype = rrtype.upper()
 
 	if rrtype == 'A':
 		try:
-			socket.inet_aton(content)
+			socket.inet_pton(socket.AF_INET, content)
 		except socket.error:
 			return False
 		return True
+
+	elif rrtype == 'AAAA':
+		try:
+			socket.inet_pton(socket.AF_INET6, content)
+		except socket.error:
+			return False
+		return True
+
+	elif rrtype == 'TXT':
+		if re_txt.search(content):
+			return True
+		else:
+			return False
 
 	else:
 		return False
